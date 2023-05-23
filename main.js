f53e415b16c35dbe21ad274c3b9ada2e13d93bcc
JNikolo <125705821+JNikolo@users.noreply.github.com>
2023-05-23 17:52:32 -0400

    Starting to organize ideas

---
+++ main.js
@@ -0,0 +1,6 @@
+// Ideas
+// 1. Calculator can do simple operations like +, -, *, / and %. 
+// 2. We can reset the calculator by pressing C
+// 3. We can use parenthesis to separate computations
+// 4. We can handle decimals by using a point. (just one point per operand)
+// 5. Result will be displayed after user clicks equal sign
